cd731a53741bf83e5b3730d71bd1090b9b01ce31
nirmal-crypto <70321846+nirmal-crypto@users.noreply.github.com>
2024-02-11 15:02:14 +0000

    termp

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ url = f'https://www.google.com/search?q=weather+sri+lanka+{query}'
 
 r = s.get(url, headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'})
 
-print(r.html.find('title', first=True).text)
+temp = r.html.find('span#wob_tm', first=True).text
