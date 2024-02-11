05ce1e34a5328cb5df83d02946471241849c9eff
nirmal-crypto <70321846+nirmal-crypto@users.noreply.github.com>
2024-02-11 14:58:24 +0000

    Initiated - weather in sri lanka

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-print('Hello World!')
-print('test commit')
-print('update')
-print('Hello World!')
-print('test commit')
-print('update')
+from requests_html import HTMLSession, user_agent
+
+s = HTMLSession()
+
+query = 'srilanka'
+url = f'https://www.google.com/search?q=weather+sri+lanka+{query}'
+
+r = s.get(url, headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'})
+
+print(r.html.find('title', first=True).text)
