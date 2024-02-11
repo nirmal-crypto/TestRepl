b64ea34ef78aa7ed33ddf209e2a6c5d026e1dc12
nirmal-crypto <70321846+nirmal-crypto@users.noreply.github.com>
2024-02-11 18:54:43 +0000

    --

--- main.py
+++ main.py
@@ -8,3 +8,5 @@ url = f'https://www.google.com/search?q=weather+sri+lanka+{query}'
 r = s.get(url, headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'})
 
 temp = r.html.find('span#wob_tm', first=True).text
+
+print(r.html.find('div.vk_bk wob-unit', first=True))
